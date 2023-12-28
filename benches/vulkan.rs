6ab6ad3a8fb000f7780d3d630ac620aab0ae57b2
Christian Döring <christian.doering@tum.de>
2023-12-28 12:16:43 +0100

    Added criterion

---
+++ benches/vulkan.rs
@@ -0,0 +1,6 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+pub fn crieterion_benchmark(c: &mut Criterion) {}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
