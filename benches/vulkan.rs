eebb7d511aebb38d38a724545f838a15d050b8b7
Christian Döring <christian.doering@tum.de>
2023-12-28 12:19:16 +0100

    Fixed typos

--- benches/vulkan.rs
+++ benches/vulkan.rs
@@ -1,6 +1,6 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-pub fn crieterion_benchmark(c: &mut Criterion) {}
+pub fn criterion_benchmark(c: &mut Criterion) {}
 
 criterion_group!(benches, criterion_benchmark);
 criterion_main!(benches);
