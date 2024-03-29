0b673d322615bb9164db32b7f21f613759272d78
Christian Döring <christian.doering@tum.de>
2024-03-29 16:41:15 +0100

    Export more types

--- hephaestus-rust/src/lib.rs
+++ hephaestus-rust/src/lib.rs
@@ -10,5 +10,6 @@ pub use texture::Texture;
 pub use var::{
     arr, array, composite, dyn_index, index, literal, mat2, mat3, mat4, sized_index, sized_literal,
     vec2, vec3, vec4, Float16, Float32, Float64, Int16, Int32, Int64, Int8, UInt16, UInt32, UInt64,
-    UInt8, Var, Vector2d, Vector2f, Vector3d, Vector3f, Vector4d, Vector4f,
+    UInt8, Var, Vector2, Vector2d, Vector2f, Vector3, Vector3d, Vector3f, Vector4, Vector4d,
+    Vector4f,
 };
